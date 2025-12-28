162ee27d4f3d7e8e34a41e80771f65c9a629ca8c
stsvt <taras.stasiv3@gmail.com>
2025-12-28 22:18:15 +0200

    feat: add schema middlewares for slug generation and active filtering

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -57,7 +57,7 @@ exports.addExpense = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent',
+      message: err.message,
     });
   }
 };
