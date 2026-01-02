93dd9cb2c9d50436dead8b1efac45c49ce2955a1
stsvt <taras.stasiv3@gmail.com>
2026-01-02 23:02:36 +0200

    feat: enhance error handling with global error handler

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const expenseRouter = require('./routes/expenseRoutes');
 
 const app = express();
@@ -7,9 +10,17 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
+
 app.set('query parser', 'extended');
 
 app.use('/api/v1/expenses', expenseRouter);
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
