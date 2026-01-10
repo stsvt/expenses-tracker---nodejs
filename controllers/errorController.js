6ad201ef4e2026769372a9beeccd1facf66298e5
stsvt <taras.stasiv3@gmail.com>
2026-01-10 12:18:29 +0200

    feat: add handling for duplicate field errors in error controller

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,12 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/).at(0);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -40,6 +46,10 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') {
       error = handleCastErrorDB(error);
+    }
+
+    if (error.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
     }
     sendErrorProd(error, res);
   }
