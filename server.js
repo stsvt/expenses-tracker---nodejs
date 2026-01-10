9abe43461214dcc88b728d09c72193b6a87d2f40
stsvt <taras.stasiv3@gmail.com>
2026-01-10 13:37:43 +0200

    feat: add uncaught exception handling to server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -7,7 +13,6 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
-
 mongoose
   .connect(DB)
   .then(() => console.log('Server was successfully connected to database'));
